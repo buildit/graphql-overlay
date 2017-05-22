9f8c61579945dea1bb1af3fedabc3bd7a0cb2d4a
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-05-22 14:51:59 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import koa from 'koa' // koa@2
+import koaRouter from 'koa-router' // koa-router@next
+import koaBody from 'koa-bodyparser' // koa-bodyparser@next
+import { graphqlKoa, graphiqlKoa } from 'graphql-server-koa'
+
+const app = new koa()
+const router = new koaRouter()
+const PORT = 3000
+
+// koaBody is needed just for POST.
+app.use(koaBody())
+
+// router.post('/graphql', graphqlKoa({ schema: myGraphQLSchema }))
+// router.get('/graphql', graphqlKoa({ schema: myGraphQLSchema }))
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.listen(PORT)
