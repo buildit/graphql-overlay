19e3e975ac19aa08b200abad4a7823008684b6e4
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-05-22 16:43:40 +0100

    Ad basic graph exploration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import koa from 'koa' // koa@2
 import koaRouter from 'koa-router' // koa-router@next
 import koaBody from 'koa-bodyparser' // koa-bodyparser@next
 import { graphqlKoa, graphiqlKoa } from 'graphql-server-koa'
+import schema from './src/schema'
 
 const app = new koa()
 const router = new koaRouter()
@@ -10,8 +11,8 @@ const PORT = 3000
 // koaBody is needed just for POST.
 app.use(koaBody())
 
-// router.post('/graphql', graphqlKoa({ schema: myGraphQLSchema }))
-// router.get('/graphql', graphqlKoa({ schema: myGraphQLSchema }))
+router.post('/graphql', graphqlKoa({ schema: schema }))
+router.get('/graphql', graphqlKoa({ schema: schema }))
 router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 
 app.use(router.routes())
